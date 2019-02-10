afb4201519795c74a7d456be4b8da518ee9ea9c1
htoopyae <htoopyae@burmese.ai>
2019-02-10 15:56:55 +0630

    actions on btns and json preview added

--- src/components/FormBuilder.js
+++ src/components/FormBuilder.js
@@ -4,6 +4,24 @@ import LangBtns from './LangBtns/LangBtns';
 import Input from './Input/Input';
 import './FormBuilder.css';
 
+const defaultElement = {
+  value: '',
+  type: 'text',
+  placeholder: 'Write your question',
+  option: {
+    type: null,
+    items: []
+  },
+  error: {
+    placeholder: 'Write your error message',
+    errorMessage: ''
+  }
+};
+
+const defaultItem = {
+  name: '', value: 'New Item', text: 'New Item'
+};
+
 class FormBuilder extends Component {
   constructor(props) {
     super(props);
@@ -12,26 +30,35 @@ class FormBuilder extends Component {
       selectedLanguage: 'en',
       elements: [
         {
-          question: 'Write Your Question',
+          value: '',
           type: 'text',
-          options: [],
-          errorMsg: 'Error!!!'
+          placeholder: 'Write your question',
+          option: {
+            type: null,
+            items: [
+              {
+                name: '', value: 'New Item', text: 'New Item'
+              }
+            ]
+          },
+          error: {
+            placeholder: 'Write your error message',
+            errorMessage: ''
+          }
         }
       ]
     };
 
     this.addElement = this.addElement.bind(this);
     this.removeElement = this.removeElement.bind(this);
+    this.onChangeLanguage = this.onChangeLanguage.bind(this);
+    this.inputChangedHandler = this.inputChangedHandler.bind(this);
+    this.optionAddedHandler = this.optionAddedHandler.bind(this);
+    this.addOptionItem = this.addOptionItem.bind(this);
+    this.itemInputChangedHandler = this.itemInputChangedHandler.bind(this);
   }
 
   addElement() {
-    const defaultElement = {
-      question: 'Write Your Question',
-      type: 'text',
-      options: [],
-      errorMsg: 'Error!!!'
-    };
-
     this.setState({
       elements: [...this.state.elements, defaultElement]
     });
@@ -48,23 +75,94 @@ class FormBuilder extends Component {
     this.setState({ selectedLanguage: language });
   }
 
+  inputChangedHandler(event, elemIndex) {
+    const updatedElements = [...this.state.elements];
+    const updatedElement = { ...updatedElements[elemIndex] };
+
+    updatedElement.value = event.target.value;
+    updatedElements[elemIndex] = updatedElement;
+
+    this.setState({ elements: updatedElements });
+  }
+
+  errorInputChangedHandler(event, elemIndex) {
+    const updatedElements = [...this.state.elements];
+    const updatedElement = { ...updatedElements[elemIndex] };
+
+    updatedElement.error.errorMessage = event.target.value;
+    updatedElements[elemIndex] = updatedElement;
+
+    this.setState({ elements: updatedElements });
+  }
+
+  optionAddedHandler(optionType, elemIndex) {
+    const updatedElements = [...this.state.elements];
+    const updatedElement = { ...updatedElements[elemIndex] };
+
+    updatedElement.option.type = optionType;
+    updatedElement.option.items = [defaultItem];
+    updatedElements[elemIndex] = updatedElement;
+
+    this.setState({ elements: updatedElements });
+  }
+
+  addOptionItem(elemIndex) {
+    const updatedElements = [...this.state.elements];
+    const updatedElement = { ...updatedElements[elemIndex] };
+    const updatedElementOptionItems = [
+      ...updatedElement.option.items, defaultItem
+    ];
+    
+    updatedElement.option.items = updatedElementOptionItems;
+    updatedElements[elemIndex] = updatedElement;
+
+    this.setState({ elements: updatedElements });
+  }
+
+  itemInputChangedHandler(event, elemIndex, itemIndex) {
+    console.log(elemIndex, itemIndex);
+
+    // const updatedElements = [...this.state.elements];
+    // const updatedElement = { ...updatedElements[elemIndex] };
+    // const updatedElementOptionItems = [...updatedElement.option.items];
+    
+    // updatedElementOptionItems[itemIndex].name = event.target.name;
+    // updatedElementOptionItems[itemIndex].value = event.target.value;
+    // updatedElementOptionItems[itemIndex].text = event.target.text;
+
+    // updatedElement.option.items = updatedElementOptionItems;
+    // updatedElements[elemIndex] = updatedElement;
+
+    // this.setState({ elements: updatedElements });
+  }
+
   render() {
     const elements = this.state.elements;
 
     return (
       <div className='Container'>
-        <LangBtns selectedLanguage={this.state.selectedLanguage} 
-          changeLanguage={this.onChangeLanguage}
-        />
-        <div className='Form'>
-          {elements.length > 0 && elements.map((elem, i) => 
-            <Input key={i} type={elem.type} question={elem.question}
-              removeElement={() => this.removeElement(i)}
-            />
-          )}
+        <div className='Left'>
+          <LangBtns selectedLanguage={this.state.selectedLanguage} 
+            changeLanguage={this.onChangeLanguage}
+          />
+          <div className='Form'>
+            {elements.length > 0 && elements.map((elem, i) => 
+              <Input key={i} 
+                config={elem}
+                removeElement={() => this.removeElement(i)}
+                changed={(e) => this.inputChangedHandler(e, i)}
+                errorChanged={(e) => this.errorInputChangedHandler(e, i)}
+                addOption={(e) => this.optionAddedHandler(e, i)}
+                addOptionItem={() => this.addOptionItem(i)}
+              />
+            )}
+          </div>
+          <div className='Btns'>
+            <button onClick={this.addElement} className='Btn Btn-circle'><AddIcon size='large' /></button>
+          </div>
         </div>
-        <div className='Btns'>
-          <button onClick={this.addElement} className='Btn Btn-circle'><AddIcon /></button>
+        <div className='Right'>
+          <pre className='Card'>{JSON.stringify(this.state, null, 2)}</pre>
         </div>
       </div>
     );
--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import CrossCircleIcon from '@atlaskit/icon/glyph/cross-circle';
-import StatusIcon from '@atlaskit/icon/glyph/status';
+import AddCircleIcon from '@atlaskit/icon/glyph/add-circle';
 import EditorBulletListIcon from '@atlaskit/icon/glyph/editor/bullet-list';
 import TaskIcon from '@atlaskit/icon/glyph/task';
 import EmojiProductivityIcon from '@atlaskit/icon/glyph/emoji/productivity';
+import Option from '../Option/Option';
 import './Input.css';
 
 const Input = props => {
+  const optionType = props.config.option.type;
   let inputElement;
-  let onErrorShowMessage = <input type='text' placeholder='If error, show this' />;
+  let onErrorShowMessage = <input type='text'
+    placeholder={props.config.error.placeholder}
+    value={props.config.error.errorMsg}
+    style={{ marginLeft: '2rem' }}
+    onChange={props.errorChanged} />;
   
-  switch(props.type) {
+  switch(props.config.type) {
     case 'text':
-      inputElement = <input type='text' placeholder={props.question} />;
+      inputElement = <input type='text'
+        placeholder={props.config.placeholder}
+        value={props.config.value}
+        onChange={props.changed} />;
       break;
     case 'select':
       inputElement = <h1>Select Options</h1>
@@ -29,11 +38,17 @@ const Input = props => {
       <div onClick={props.removeElement} className='Del-element'><CrossCircleIcon /></div>
       <div className='Input'>{inputElement} {onErrorShowMessage}</div>
       <div className='Actions'>
-        <button className='Btn' onClick={() => alert('hello')}><StatusIcon /></button>
-        <button className='Btn' onClick={() => alert('hello')}><EditorBulletListIcon /></button>
-        <button className='Btn' onClick={() => alert('hello')}><TaskIcon /></button>
-        <button className='Btn' onClick={() => alert('hello')}><EmojiProductivityIcon /></button>
+        <p>Choose one of reply options</p>
+        <button className='Btn' onClick={() => props.addOption('select')}><EditorBulletListIcon /></button>
+        <button className='Btn' onClick={() => props.addOption('checkbox')}><TaskIcon /></button>
+        <button className='Btn' onClick={() => props.addOption('radio')}><EmojiProductivityIcon /></button>
       </div>
+      {optionType && <div className='Options'>
+        <Option type={optionType} items={props.config.option.items} />
+        <button className='Btn' onClick={() => props.addOptionItem(optionType)}>
+          <AddCircleIcon size='large' />
+        </button>
+      </div>}
     </div>
   );
 }
